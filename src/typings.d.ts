fa4e77ef2c437c6698273fa9398c4618a021dc7c
hirsty777 <777hirsty@gmail.com>
2023-12-29 21:15:38 +0400

    fix

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,5 +1,7 @@
 declare module "*.module.css";
 declare module '*.svg' {
-    const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
+    const content: any;
     export default content;
   }
+declare module '*.jpg';
+declare module '*.png';
